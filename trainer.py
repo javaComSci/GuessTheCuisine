efe620b9266858215d9534fa79961bd0c34d489d
javaComSci <iramanat@purdue.edu>
2020-06-24 20:41:00 -0400

    Split data into training and testing sets

--- trainer.py
+++ trainer.py
@@ -1,13 +1,32 @@
 import pandas as pd
 import numpy as np
 import pickle
+from logisticRegression import LogisticRegression
 
 
 """
     Split the data into training and testing sets
 """
 def splitData():
+    # read the pickled file
     df = pd.read_pickle("recipies_cleaned.pkl")
+
+    # randomly shuffle the rows
+    df = df.sample(frac=1)
+
+    # say that 80% of the samples are for training and rest is for testing
+    trainingSize = int(0.8 * len(df))
+    # print(trainingSize, len(df))
+
+    # split into training and testing sets
+    trainingX = df.loc[:, df.columns != "cuisine"][:trainingSize]
+    trainingY = df.iloc[:,0][:trainingSize]
+    testingX = df.loc[:, df.columns != "cuisine"][trainingSize + 1:]
+    testingY = df.iloc[:,0][trainingSize + 1:]
+    print(len(testingY), testingY)
+
+    return trainingX, trainingY, testingX, testingY
+
 
 
 if __name__ == "__main__":
